38b3c4dd22f6cde5c7dae5e7a39bc47df645f1cd
stlouisn <stlouisn@hotmail.com>
2024-09-05 10:49:55 -0300

    Update ModName.java

--- src/main/java/com/github/mod_id/ModName.java
+++ src/main/java/com/github/mod_id/ModName.java
@@ -1,20 +1,19 @@
-package com.github.mortar;
+package maven_name.mod_id;
 
+import maven_name.mod_id.config.ModConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.ItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModName implements ModInitializer {
 
-  public static final String MOD_ID = "mortar";
+  public static final String MOD_ID = "mod_id";
 
   @SuppressWarnings("unused")
   public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
   @Override
   public void onInitialize() {
+    Config.init();
   }
 }
