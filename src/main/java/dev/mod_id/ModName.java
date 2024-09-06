acf3d03128d7a08e5eaa7bc77c0bd17a519451a4
stlouisn <stlouisn@hotmail.com>
2024-09-05 22:50:29 -0300

    Update and rename src/main/java/com/github/mod_id/PackageName.java to src/main/java/dev/mod_id/ModName.java

--- src/main/java/dev/mod_id/ModName.java
+++ src/main/java/dev/mod_id/ModName.java
@@ -1,18 +1,16 @@
 package maven_name.mod_id;
 
-import maven_name.mod_id.config.ModConfig;
+import dev.mod_id.config.ModConfig;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModName implements ModInitializer {
 
-  public static final String MOD_ID = "mod_id";
-
   @SuppressWarnings("unused")
-  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
-  @Override
   public void onInitialize() {
+    ModConfig.init();
   }
 }
