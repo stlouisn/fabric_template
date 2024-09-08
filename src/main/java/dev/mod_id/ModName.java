7f73e5a0c75652bd726409286ca48d22607c1b6f
stlouisn <stlouisn@hotmail.com>
2024-09-08 14:47:28 -0300

    Update ModName.java

--- src/main/java/dev/mod_id/ModName.java
+++ src/main/java/dev/mod_id/ModName.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModName implements ModInitializer {
+public final class ModName implements ModInitializer {
 
   @SuppressWarnings("unused")
   public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
