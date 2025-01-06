1fa03de453cde2cb173f8bf9da879b0e6346da6f
stlouisn <stlouisn@hotmail.com>
2025-01-06 13:38:00 -0400

    Update ModName.java

--- src/main/java/dev/mod_id/ModName.java
+++ src/main/java/dev/mod_id/ModName.java
@@ -8,8 +8,10 @@ import org.slf4j.LoggerFactory;
 
 public final class ModName implements ModInitializer {
 
+  public static final String MOD_ID = "mod_id";
+
   @SuppressWarnings("unused")
-  public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
   public void onInitialize() {
     ModConfig.init();
