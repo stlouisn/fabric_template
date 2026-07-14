17d451a9a23c1f2423d588f98ca1dc9427bcbcc5
stlouisn <stlouisn@hotmail.com>
2026-07-14 08:04:50 -0300

    fabric-template

---
+++ src/main/java/com/mod_id/ModNameClient.java
@@ -0,0 +1,10 @@
+package com.mod_id;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ModNameClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
