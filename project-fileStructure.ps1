066ed2cfc4d6d5708bf63e25b9d66d154104de3a
stlouisn <stlouisn@hotmail.com>
2026-07-18 11:11:39 -0300

    fabric-template

--- project-fileStructure.ps1
+++ project-fileStructure.ps1
@@ -2,7 +2,6 @@
 
 tree .\src /F /A |
     Select-String -NotMatch "^(Folder PATH|Volume serial|[A-Z]:\\)" |
-    Select-String -NotMatch "\.cache" |
     Add-Content project-FileStructure.md
 
 '```' | Add-Content project-fileStructure.md
