a5dfca870884210ca84ced84c46973d05f12469b
stlouisn <stlouisn@hotmail.com>
2026-07-25 12:14:16 -0300

    fabric-26.2

--- project-fileStructure.ps1
+++ project-fileStructure.ps1
@@ -1,11 +1,17 @@
 # Filename
 $File = "file-structure.md"
 
+# Delete cache folder if it exists
+$CachePath = "src/generated/resources/.cache"
+if (Test-Path $CachePath) {
+    Remove-Item $CachePath -Recurse -Force
+}
+
 # Write header
 '```plaintext' | Set-Content $File
 
 # Write filtered tree output
-tree .\src /F /A |
+tree ./src /F /A |
     Select-String -NotMatch "^(Folder PATH|Volume serial|[A-Z]:\\)" |
     Add-Content $File
 
