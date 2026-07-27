80706cb4eccada8a973737a9ab25c820993aacc8
stlouisn <stlouisn@hotmail.com>
2026-07-27 15:12:33 -0300

    fabric-26.2

--- src/main/java/com/mod_id/datagen/provider/DatagenBlockLootSubProvider.java
+++ src/main/java/com/mod_id/datagen/provider/DatagenBlockLootSubProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
@@ -55,5 +56,14 @@ public final class DatagenBlockLootSubProvider extends FabricBlockLootSubProvide
         Identifier identifier = resourceLocation(namespace, "blocks/" + path);
         LootTable.Builder builder = this.createSilkTouchDispatchTable(block, LootItem.lootTableItem(drops).apply(SetItemCountFunction.setCount(ConstantValue.exactly(4.0F))).apply(ApplyBonusCount.addOreBonusCount(this.registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE))).when(MatchTool.toolMatches(ItemPredicate.Builder.item().of(this.registries.lookupOrThrow(Registries.ITEM), ItemTags.CLUSTER_MAX_HARVESTABLES))).otherwise(this.applyExplosionDecay(block, LootItem.lootTableItem(drops).apply(SetItemCountFunction.setCount(ConstantValue.exactly(2.0F)))))).setRandomSequence(identifier);
         this.add(block, builder);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private void createRawOreDrop(final Block original, final Item drop) {
+        String namespace = BuiltInRegistries.BLOCK.getKey(original).getNamespace();
+        String path = BuiltInRegistries.BLOCK.getKey(original).getPath();
+        Identifier identifier = resourceLocation(namespace, "blocks/" + path);
+        LootTable.Builder builder = this.createSilkTouchDispatchTable(original, LootItem.lootTableItem(drop).apply(ApplyBonusCount.addOreBonusCount(this.registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE)))).setRandomSequence(identifier);
+        this.add(original, builder);
     }
 }
