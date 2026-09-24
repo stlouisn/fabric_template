3827efc4c3422d015cdfce6a8a00138ee88ebea7
stlouisn <stlouisn@hotmail.com>
2026-09-24 15:54:31 -0300

    update

--- project-update.ps1
+++ project-update.ps1
@@ -68,7 +68,7 @@ if (-not (Test-Path -Path ".\$ScriptFilename")) {
 # Compute hash after update
 $AfterHash = (Get-FileHash -Path $Self -Algorithm SHA256).Hash
 
-# If the script updated itself, ?
+# Exit if the script updated itself
 if ($BeforeHash -ne $AfterHash) {
     Write-Host
     Write-Host "Script updated. You will need to restart it." -ForegroundColor Red
@@ -140,6 +140,25 @@ $gradleVersions = Invoke-RestMethod "https://services.gradle.org/versions/all"
 
 # Find the latest stable release
 $gradleLatest = ($gradleVersions | Where-Object { $_.current -eq $true }).version
+
+# Graddle Wrapper
+$wrapperJar = ".\gradle\wrapper\gradle-wrapper.jar"
+
+# If wrapper is missing, bootstrap Gradle
+if (-not (Test-Path $wrapperJar)) {
+    Write-Host
+    Write-Host "gradle-wrapper.jar missing. Bootstrapping Gradle..." -ForegroundColor Yellow
+    $zipFile = Join-Path $env:TEMP "gradle-$gradleLatest-bin.zip"
+    $extractPath = Join-Path $env:TEMP "gradle-$gradleLatest"
+    if (-not (Test-Path $extractPath)) {
+        Invoke-WebRequest -Uri "https://services.gradle.org/distributions/gradle-$gradleLatest-bin.zip" -OutFile $zipFile
+        Expand-Archive -Path $zipFile -DestinationPath $env:TEMP -Force
+    }
+    & "$extractPath\bin\gradle.bat" wrapper --gradle-version $gradleLatest --distribution-type bin
+    if (-not (Test-Path $wrapperJar)) {
+        throw "Failed to generate gradle-wrapper.jar"
+    }
+}
 
 # Update the Gradle Wrapper to the latest version
 ./gradlew --console=colored wrapper --gradle-version $gradleLatest --distribution-type bin
